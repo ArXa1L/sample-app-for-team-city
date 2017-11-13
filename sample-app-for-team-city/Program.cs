e6f84e20c1f1fb66ce5e190e5283e4925d15eb51
Denis Tarasov <dstarasov@kontur.ru>
2017-11-13 10:51:46 +0500

    added 5 warnings

--- sample-app-for-team-city/Program.cs
+++ sample-app-for-team-city/Program.cs
@@ -11,6 +11,12 @@ namespace sample_app_for_team_city
 
             var hello = "Hello, ";
             Console.WriteLine(hello+" ConvertToConstant.Local inspection");
+
+            var a1 = "";
+            var a2 = "";
+            var a3 = "";
+            var a4 = "";
+            var a5 = "";
             
             Console.WriteLine("Hello, TeamCity");
         }
