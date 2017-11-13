f8148944f87787f62a20b73f1ff593eff32e5896
Denis Tarasov <dstarasov@kontur.ru>
2017-11-13 10:45:53 +0500

    created sample app

---
+++ sample-app-for-team-city/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sample_app_for_team_city
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, TeamCity");
+        }
+    }
+}
